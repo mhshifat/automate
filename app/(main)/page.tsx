2e881973af5d564eb0d7be10279e587e10e3e66d
shifat.dev <adnan@lazertechnologies.com>
2024-06-03 21:15:36 +0600

    done

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function HomePage() {
-  return (
-    <div>
-      <p>Welcome</p>
-    </div>
-  )
+  return redirect("/dashboard");
 }
